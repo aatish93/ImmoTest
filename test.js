5d7bd194be07d9bc16093cafd25669803fbc4df5
aatish93 <aatish93@gmail.com>
2018-07-26 16:15:23 +0530

    Structure fix added.

--- test.js
+++ test.js
@@ -28,10 +28,12 @@ function writeToFile(flatDetails){
 
 function getFlatData(body){
   var respBody = cheerio.load(body);
-  var contentName,contentNoOfRooms,contentLocality,i,j,listOfNames=new Array(),listOfRoomNo=new Array(),listOfLocality=new Array(),len,finalOutput="";
+  var contentName,contentNoOfRooms,contentLocality,contentPrice,contentFloorSize,i,j,listOfNames=new Array(),listOfRoomNo=new Array(),listOfLocality=new Array(),listOfPrices=new Array(),listOfFloorSize=new Array(),len,finalOutput="City; Number of rooms; Floor Size; Price\n";
   var propertryNames = respBody(".m-srp-card__title__bhk");
   var noOfRooms=respBody("*[itemprop = 'numberOfRooms']");
   var locality=respBody("*[itemprop = 'addressLocality']");
+  var price=respBody(".m-srp-card__price");
+  var floorSize=respBody("*[itemprop = 'floorSize']");
   j=0;
   for(i=0;i<propertryNames.length;i++){
 	  contentName = respBody(propertryNames[i]).text();
@@ -55,8 +57,25 @@ function getFlatData(body){
 	  listOfLocality[j]=contentLocality;
 	  j++;
   }
+  j=0;
+  for(i=0;i<(len*2);i=i+2){
+	  contentPrice=respBody(price[i]).text();
+	  if (isNaN(parseInt(contentPrice.charAt(0)))){
+	  	i--;
+	  	contentPrice=contentPrice.substring(1,contentPrice.length - 1);
+	  }
+	  listOfPrices[j]=contentPrice;
+	  j++;
+  }
+  j=0;
   for(i=0;i<len;i++){
-	  finalOutput=finalOutput + (listOfNames[i] +";Number of Rooms:"+ listOfRoomNo[i] +";Address Locality:"+ listOfLocality[i] +"\n");
+	  contentFloorSize=respBody(floorSize[i]).attr('content');
+	  listOfFloorSize[j]=contentFloorSize;
+	  j++;
+  }
+  for(i=0;i<len;i++){
+	  finalOutput=finalOutput + (listOfLocality[i] +"; "+ listOfRoomNo[i]+"; "+ listOfFloorSize[i] +"; "+ listOfPrices[i]+"\n");
+  		
   }
   return finalOutput;
 }
